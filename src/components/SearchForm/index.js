ee2bef3ab6dda1dff448a00e6c6927d97ed3e02d
arseniyasokolov <arseniy.a.sokolov@gmail.com>
2020-04-05 17:41:34 +0500

    add draft of Bricks library

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Input, ControlWrapper, Icons, Icon } from '../../bricks';
 import { useDebouncedInput } from '../../hooks/useDebouncedInput';
 
 export const SearchForm = ({ onUpdate }) => {
@@ -7,7 +8,14 @@ export const SearchForm = ({ onUpdate }) => {
 
     return (
         <form>
-            <input placeholder='Например, COVID-19' value={value} onChange={({ target: { value } }) => update(value)} />
+            <ControlWrapper
+                label='Введите фразу для поиска'
+                hint='Поиск в функциональном стиле'
+            >
+                <Input placeholder='Например, COVID-19' value={value} onChange={update}>
+                    <Icon>{Icons.Search}</Icon>
+                </Input>
+            </ControlWrapper>
         </form>
     );
 };
